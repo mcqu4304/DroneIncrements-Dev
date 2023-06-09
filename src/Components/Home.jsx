b6e119ad9b2612170512be213e7747a62eeb29c5
Maria <m.mcquillan290@gmail.com>
2023-06-09 18:21:05 -0500

    bring site up to date with links and table. add skeleton coming soon page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import React from 'react';
+import drone from "../Images/droneIcon.svg";
+import NavBar from "../Components/NavBar";
+import IncrementTable from '../Components/IncrementTable';
+
+class Home extends React.Component {
+    render() {
+        return (       
+            <div className="App">
+            <div className='align-logo'>       
+                <h1>
+                <img
+                alt=""
+                src={drone}
+                width="50"
+                height="50"
+                className="d-inline-block"
+                />{' '}
+                  Drone Increments</h1>
+            </div>
+            <NavBar></NavBar>
+            <div className='align-table'>
+              <div style={{paddingBottom: 50}}>
+              <h3>Problem</h3>
+              <p>I want to fly RC aircraft (Unmanned aircraft system) I don't know what I need, what's available, and in my budget.</p>
+              <h3>Solution</h3>
+              <p> Drone Increments, a  shared collection of parts and builds. Helping all of us understand different ways we can reach our own fun flight goals!</p>
+              </div>
+            <h2 style={{paddingBottom: 10}}>Recommended Builds</h2>
+            <IncrementTable></IncrementTable>
+            </div>
+            <div>
+                  <p style={{position: "relative", bottom: 0, width:"100%", textAlign: "center"}}> Icons from <a className ="App-link" href="https://www.svgrepo.com/svg/170240/drone" title="Flaticon"> https://www.svgrepo.com</a>
+                  </p>
+            </div>
+          </div>
+        );
+    }
+
+}
+
+export default Home;
